6883372b58e710bd1991fdeb392c67f69e0efb2c
joaolucas3002 <92446800+joaolucas3002ht@users.noreply.github.com>
2023-04-08 16:41:10 -0300

    remuve routes from api router

--- src/app/api/exit-preview/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export  async function GET(req: NextApiRequest, res: NextApiResponse) {
-   res.clearPreviewData();
-   res.writeHead(307, { Location: '/' });
-   res.end();
-}
