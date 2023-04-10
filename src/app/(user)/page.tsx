bb1dfececdbc38265f6268fbd7f2374821c7e079
joaolucas3002 <92446800+joaolucas3002ht@users.noreply.github.com>
2023-04-10 15:28:42 -0300

    Update page.tsx

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -20,13 +20,13 @@ export default function Login() {
    const Posts = '';
 
    return (
-      <div className="w-full flex flex-col gap-8 max-w-3xl m-auto relative h-full px-[min(4%_,_2rem)] py-12">
+      <div className="w-full flex flex-col gap-[clamp(20px,4vw,32px)] max-w-3xl m-auto relative h-full px-[min(4%_,_2rem)] py-12">
          <h1 className="font-bold text-[clamp(1.875rem,8vw,45px)] text-center text-sky-700 font-mono">
             EREM Antonio Inacio
          </h1>
 
          <div
-            className={`w-[min(100%_,_45rem)] h-[min(56.25vw_,_27rem)] relative mx-auto rounded-2xl overflow-hidden bg-gray-500/90 mb-[clamp(10px,3vw,20px)]`}
+            className={`w-[min(100%_,_45rem)] h-[min(56.25vw_,_27rem)] relative mx-auto rounded-2xl overflow-hidden bg-gray-500/90`}
          >
             {/* <Image
                   src={urlFor(mainImage).url()}
@@ -36,7 +36,7 @@ export default function Login() {
                /> */}
          </div>
 
-         <section className=' flex flex-col gap-6'>
+         <section className=' flex flex-col gap-4'>
            <h2 className="capitalize font-semibold text-3xl text-sky-700  font-mono">
               ultimas postagens
            </h2>
@@ -44,7 +44,7 @@ export default function Login() {
            <CarouselPosts />
          </section>
 
-         <section className=' flex flex-col gap-6'>
+         <section className=' flex flex-col gap-4'>
            <h2 className="capitalize font-semibold text-3xl text-sky-700  font-mono">
               Endereço
            </h2>
