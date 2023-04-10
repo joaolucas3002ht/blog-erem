2267a0daab7bef1e5c12a258108237427d40187a
joaolucas3002 <92446800+joaolucas3002ht@users.noreply.github.com>
2023-04-10 15:36:04 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
    return (
-      <footer className="w-full border-t-2 border-slate-600">
+      <footer className="w-full border-t-2 border-slate-500/60">
          <section className="py-8 px-6 text-white flex justify-center">
             <div className="max-w-7xl w-full h-fit ">
             <Link href={"/"}>© 2023 EREM Antonio Inacio, Inc.</Link>
