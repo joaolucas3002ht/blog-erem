0a8e639ca6144d55c243e3f2c132cec838deefd1
joaolucas3002 <92446800+joaolucas3002ht@users.noreply.github.com>
2023-04-15 23:06:22 -0300

    remove accordion

--- src/components/Header/MidiaMenu.tsx
+++ src/components/Header/MidiaMenu.tsx
@@ -40,4 +40,67 @@ export function MidiaMenu({ toggle }: MidiaMenuProps) {
    );
 }
 
-
+{/* <div
+   id="accordion-flush"
+   data-accordion="collapse"
+   data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"
+   data-inactive-classes="text-gray-500 dark:text-gray-400"
+>
+   <h2 id="accordion-flush-heading-1">
+      <button
+         type="button"
+         className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+         data-accordion-target="#accordion-flush-body-1"
+         aria-expanded="true"
+         aria-controls="accordion-flush-body-1"
+      >
+         <span>What is Flowbite?</span>
+         <BiChevronDown size={20} />
+      </button>
+   </h2>
+   <div
+      id="accordion-flush-body-1"
+      className="hidden"
+      aria-labelledby="accordion-flush-heading-1"
+   >
+      <p></p>
+   </div>
+   <h2 id="accordion-flush-heading-2">
+      <button
+         type="button"
+         className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+         data-accordion-target="#accordion-flush-body-2"
+         aria-expanded="false"
+         aria-controls="accordion-flush-body-2"
+      >
+         <span>Is there a Figma file available?</span>
+         <BiChevronDown size={20} />
+      </button>
+   </h2>
+   <div
+      id="accordion-flush-body-2"
+      className="hidden"
+      aria-labelledby="accordion-flush-heading-2"
+   >
+      <p></p>
+   </div>
+   <h2 id="accordion-flush-heading-3">
+      <button
+         type="button"
+         className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+         data-accordion-target="#accordion-flush-body-3"
+         aria-expanded="false"
+         aria-controls="accordion-flush-body-3"
+      >
+         <span>What are the differences between Flowbite and Tailwind UI?</span>
+         <BiChevronDown size={20} />
+      </button>
+   </h2>
+   <div
+      id="accordion-flush-body-3"
+      className="hidden"
+      aria-labelledby="accordion-flush-heading-3"
+   >
+      <p></p>
+   </div>
+</div>; */}
