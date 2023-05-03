0792315bd6b1731243f46a01a76e0d15ffafff92
joaolucas3002 <92446800+joaolucas3002ht@users.noreply.github.com>
2023-05-03 13:11:14 -0300

    Update MainH2.tsx

--- src/components/MainH2.tsx
+++ src/components/MainH2.tsx
@@ -6,7 +6,7 @@ interface MainH2Props {
 
 export function MainH2({ children }: MainH2Props) {
    return (
-      <h2 className=" font-semibold text-[clamp(1.25rem_,_5vw,_2rem)] text-sky-700  font-mono">
+      <h2 className=" font-semibold text-[clamp(1.875rem_,_6vw,_2.625rem)] text-sky-700  font-mono">
          {children}
       </h2>
    );
